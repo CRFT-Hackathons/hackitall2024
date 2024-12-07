961b75ea1e58906e983eae499884f8d5988937b1
robertDinca2003 <robert20031114@gmail.com>
2024-12-07 21:59:30 +0200

    Updating recommandation score of an user!

--- src/lib/helpers/updateRecommandationScore.ts
+++ src/lib/helpers/updateRecommandationScore.ts
@@ -1,3 +1,8 @@
+"use server";
+import { db } from "../db";
+import { eq } from "drizzle-orm";
+
+import { users } from "../schema/user";
 const ALPHA = 0.5;
 
 /**
@@ -10,7 +15,49 @@ function calculateNextRecomanadationScore(tn: number, Tn: number): number {
   return ALPHA * tn + (1 - ALPHA) * Tn;
 }
 
-const tn = 10;
-const Tn = 8;
-const TnPlus1 = calculateNextRecomanadationScore(tn, Tn);
-console.log(`The predicted next recomandation score (Tn+1) is: ${TnPlus1}`);
+// const tn = 10;
+// const Tn = 8;
+// const TnPlus1 = calculateNextRecomanadationScore(tn, Tn);
+// console.log(`The predicted next recomandation score (Tn+1) is: ${TnPlus1}`);
+
+export async function updateUserRecomandationScoreOnScroll(
+  userID: string,
+  latestPostScore: number[]
+): Promise<void> {
+  const currentRecommandationScore = await db
+    .select({ recommand_score: users.recommand_score })
+    .from(users)
+    .where(eq(users.id, userID))
+    .then((rows) => rows[0]?.recommand_score || 0);
+
+  const newScore: number = calculateNextRecomanadationScore(
+    currentRecommandationScore as number,
+    latestPostScore.reduce((acc, curr) => acc + curr, 0) /
+      latestPostScore.length +
+      (Math.random() - 1 / 2) * 2
+  );
+  await db
+    .update(users)
+    .set({ recommand_score: String(newScore) })
+    .where(eq(users.id, userID));
+}
+
+export async function updateUserRecomandationScoreOnPost(
+  userID: string,
+  postScore: number
+): Promise<void> {
+  const currentRecommandationScore = await db
+    .select({ recommand_score: users.recommand_score })
+    .from(users)
+    .where(eq(users.id, userID))
+    .then((rows) => rows[0]?.recommand_score || 0);
+  console.error(currentRecommandationScore, typeof currentRecommandationScore);
+  const newScore: number = calculateNextRecomanadationScore(
+    currentRecommandationScore as number,
+    postScore
+  );
+  await db
+    .update(users)
+    .set({ recommand_score: String(newScore) })
+    .where(eq(users.id, userID));
+}
